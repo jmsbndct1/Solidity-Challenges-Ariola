6a7b0d62c5494db592c30e380c8a03709045b682
jmsbndct1 <127822400+jmsbndct1@users.noreply.github.com>
2023-06-12 04:09:19 -1100

    Update Solidity -Challenge1.sol

--- Solidity -Challenge1.sol
+++ Solidity -Challenge1.sol
@@ -1,41 +1,51 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-// James Benedict Ariola BSIT 3.1 
+pragma solidity >=0.8.18;
 
-contract Chall1Contract {
-    
-    uint public uintVariable;
-    bool public boolVariable;
-    string public stringVariable;
-    address public addressVariable;
+contract Challenge_Contract {
+    // Declare variables
+    uint public numVar;
+    string public stringVar;
+    bool public boolVar;
+    address public addressVar;
 
-    // ----------------------Set functions of chosen variables----------------
-    function setUintVariable(uint256 _value) public {
-        uintVariable = _value;
+    // Set the value of numVar and return the updated value
+    function setnumVar(uint _number) public returns (uint) {
+        numVar = _number;
+        return numVar;
     }
-    function setBoolVariable(bool _value) public {
-        boolVariable = _value;
+    // Get the value of numVar
+    function getnumVar() public view returns (uint) {
+        return numVar;
     }
 
-    function setStringVariable(string memory _value) public {
-        stringVariable = _value;
+    // Set the value of stringVar and return the updated value
+    function setstringVar(string memory _string) public returns (string memory) {
+        stringVar = _string;
+        return stringVar;
     }
-    function setAddressVariable(address _value) public {
-        addressVariable = _value;
-    }
-    // ----------------------------end of set functions-----------------------
 
-    // Get functions for each variable
-    function getUintVariable() public view returns (uint256) {
-        return uintVariable;
+    // Get the value of stringVar
+    function getstringVar() public view returns (string memory) {
+        return stringVar;
     }
-    function getBoolVariable() public view returns (bool) {
-        return boolVariable;
+
+    // Set the value of boolVar and return the updated value
+    function setboolVar(bool _bool) public returns (bool) {
+        boolVar = _bool;
+        return boolVar;
     }
-    function getStringVariable() public view returns (string memory) {
-        return stringVariable;
+     // Get the value of boolVar
+    function getboolVarl() public view returns (bool) {
+        return boolVar;
     }
-    function getAddressVariable() public view returns (address) {
-        return addressVariable;
+    // Set the value of addressVar and return the updated value
+    function setaddressVar(address _address) public returns (address) {
+        addressVar = _address;
+        return addressVar;
+    }
+
+    // Get the value of addressVar
+    function getaddressVar() public view returns (address) {
+        return addressVar;
     }
 }
